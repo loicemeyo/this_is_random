1dac8bdad53d68281b5e1f1559add83bb083559a
Loice Meyo <loicemeyo@gmail.com>
2019-08-25 12:41:26 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar'
+import TypoGraphy from '@material-ui/core/Typography'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <AppBar color="primary" position="static">
+          <Toolbar>
+            <TypoGraphy variant="title"
+              color="inherit"
+            >
+             Phone number generator
+           </TypoGraphy>
+          </Toolbar>
+        </AppBar>
+
+      </div>
+    );
+  }
 }
-
 export default App;
